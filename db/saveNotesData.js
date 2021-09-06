d24c6034ad3d5d569d25d5fa8dc35c812010ad92
Derimar Gray <derimargray@gmail.com>
2021-09-05 19:32:37 -0500

    add save note and update panel notes

--- db/saveNotesData.js
+++ db/saveNotesData.js
@@ -1,6 +1,9 @@
 const util = require('util');
 const fs = require('fs');
 
+// package manager of ids
+const { v4: uuidv4 } = require('uuid');
+
 const readNote = util.promisify(fs.readFile);
 const writeNote = util.promisify(fs.writeFile);
 
@@ -23,6 +26,19 @@ class Save {
             }
             return parsedNotes;
         });
+    }
+
+    // Save notes
+    saveNote(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error('Title and text are required!');
+        }
+        // Add unique IDs
+        const newNote = { title, text, id: uuidv4() };
+
+        // Save new note and update lef panel
+        return this.retrieveNotes().then(notes => [...notes, newNote]).then(updatedNotes => this.write(updatedNotes)).then(() => newNote);
     }
 
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 
 const saveNotesData = require('../db/saveNotesData');
 
+// Get request
 router.get('/notes', function (req, res) {
     saveNotesData.retrieveNotes().then(notes => res.json(notes)).catch(err => res.status(500).json(err));
 });
 
+// Post request
 router.post('/notes', (req, res) => {
-    saveNotesData.addNote(req.body).then((note) => res.json(note)).catch(err => res.status(500).json(err));
+    saveNotesData.saveNote(req.body).then((note) => res.json(note)).catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
